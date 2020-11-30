bf9be2f9b1d5d989be3f7b2f8955182cad287cb0
mbaros <mbaros@email.com>
2020-11-30 23:56:42 +0100

    oprava web index html, upgrade gradle, implementace FilePicker pluginu

--- lib/src/ui/pages/nas/nas_sync_page.dart
+++ lib/src/ui/pages/nas/nas_sync_page.dart
@@ -1,7 +1,9 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 //  https://github.com/GIfatahTH/states_rebuilder/tree/master/examples/ex_009_1_3_ca_todo_mvc_with_state_persistence
 //https://flutter.dev/docs/cookbook/forms
+//https://github.com/miguelpruivo/flutter_file_picker/blob/master/example/lib/src/file_picker_demo.dart
 
 ///Class showing a page for setting a NAS folder
 class NASSyncMainPage extends StatefulWidget {
@@ -13,10 +15,24 @@ class NASSyncMainPage extends StatefulWidget {
 
 class _SyncPathEditFormState extends State<NASSyncMainPage> {
   final _formKey = GlobalKey<FormState>();
+  final _localFolderPathTextFieldController = TextEditingController();
 
   // Here we use a StatefulWidget to hold local fields _nasFolder and _localFolder
   String _nasFolder;
-  String _localFolder;
+
+  // String _localFolder;
+
+  @override
+  void dispose() {
+    _localFolderPathTextFieldController.dispose();
+    super.dispose();
+  }
+
+  void _selectFolder() {
+    FilePicker.platform.getDirectoryPath().then((value) {
+      setState(() => _localFolderPathTextFieldController.text = value);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,14 +65,28 @@ class _SyncPathEditFormState extends State<NASSyncMainPage> {
                 validator: (val) => val.trim().isEmpty ? 'Cannot be empty' : null,
                 onSaved: (value) => _nasFolder = value,
               ),
-              TextFormField(
-                //TODO open file manager to select a folder
-                key: Key('__LocalFolderField'),
-                initialValue: '',
-                style: Theme.of(context).textTheme.headline5,
-                decoration: InputDecoration(hintText: 'Enter local folder'),
-                validator: (val) => val.trim().isEmpty ? 'Cannot be empty' : null,
-                onSaved: (value) => _localFolder = value,
+              Row(
+                children: [
+                  SizedBox(
+                    width: 400,
+                    child: TextFormField(
+                      //TODO open file manager to select a folder
+                      key: Key('__LocalFolderField'),
+                      // initialValue: '',
+                      style: Theme.of(context).textTheme.headline5,
+                      decoration: InputDecoration(
+                        hintText: 'Enter local folder',
+                      ),
+                      validator: (val) => val.trim().isEmpty ? 'Cannot be empty' : null,
+                      controller: _localFolderPathTextFieldController,
+                      // onSaved: (value) => _localFolder = value,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => _selectFolder(),
+                    child: Text('Pick local folder'),
+                  )
+                ],
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 16.0),
@@ -69,7 +99,7 @@ class _SyncPathEditFormState extends State<NASSyncMainPage> {
                       //use local variables _nasFolder a _localFolder
                     }
                   },
-                  child: Text('Synchronizuj'),
+                  child: Text('Synchronize'),
                 ),
               ),
             ],
