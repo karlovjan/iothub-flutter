865620b01ea671dece2bf80747bf92ef35e5673d
mbaros <mbaros@email.com>
2021-08-17 23:50:33 +0200

    test iot home page 2

--- test/pages/home_page_test.dart
+++ test/pages/home_page_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:iothub/main.dart';
 import 'package:iothub/src/data_source/auth_repository.dart';
 import 'package:iothub/src/domain/entities/user.dart';
 import 'package:iothub/src/service/iothub_service.dart';
@@ -15,30 +16,56 @@ import 'home_page_test.mocks.dart';
 
 @GenerateMocks([IOTHubService, FirebaseAuthRepository])
 void main() {
+  late final mockAuth = MockFirebaseAuthRepository();
   setUp(() {
-    IOTHubsMainPage.user.injectAuthMock(() => MockFirebaseAuthRepository());
+    IOTHubsMainPage.user.injectAuthMock(() => mockAuth);
     IOTHubsMainPage.iotHubService.injectMock(() => MockIOTHubService());
   });
 
   testWidgets('display splash screen and go to home page', (tester) async {
     //IOTHubsMainPage.user.auth.signIn((param) => UserParam(signIn: SignIn.withEmailAndPassword, email: 'x@y.cz', password: 'xxx')
 
-    when(IOTHubsMainPage.user.auth.signIn((param) => param!)).thenAnswer((_) async =>
-        Future.delayed(Duration(seconds: 1)).then((_) => User(uid: '1', email: 'x@y.cz', displayName: 'test')));
+    late final testUser = User(uid: '1', email: 'x@y.cz', displayName: 'test');
 
-    await tester.pumpWidget(const HomePage());
+    when(mockAuth.currentUser()).thenAnswer((_) async => Future.value(testUser));
+    when(mockAuth.dispose()).thenAnswer((_) {
+      IOTHubsMainPage.user.auth.signOut(param: (param) => UserParam(signIn: SignIn.withEmailAndPassword));});
+    // when(IOTHubsMainPage.user.auth.injected.onAuthStream).thenReturn((_) async => Future.delayed(Duration(seconds: 1)).then((_) => testUser).asStream());
+   /* when(IOTHubsMainPage.user.auth.signIn((param) {
+      if(SignIn.withEmailAndPassword != param!.signIn){
+        throw ArgumentError('expected SignIn.withEmailAndPassword', 'param signIn');
+        }
+      if(param.email!.isEmpty ){
+        throw ArgumentError('email cannot be null', 'param email');
+    }
+
+      if(param.password!.isEmpty ){
+        throw ArgumentError('password cannot be null', 'param password');
+      }
+
+      return param;
+    })).thenAnswer((_) async =>
+        Future.delayed(Duration(seconds: 1)).then((_) => testUser));
+*/
+    await tester.pumpWidget(IOTHubApp());
 
     await tester.tap(find.byType(TileNavigationButton).at(0));
+
+    expect(find.byType(HomePage), findsOneWidget);
 
     // Rebuild the widget after the state has changed.
     await tester.pump(const Duration(milliseconds: 500));
 
     expect(find.byType(SplashScreen), findsOneWidget);
 
-    verify(IOTHubsMainPage.user.auth.signIn(any)).called(1);
 
     await tester.pumpAndSettle(const Duration(seconds: 1));
 
     expect(find.byType(IOTHubList), findsOneWidget);
+
+    verify(IOTHubsMainPage.user.auth.signIn(any)).called(1);
+    verify(mockAuth.currentUser()).called(1);
+    verifyNever(mockAuth.dispose());
+    verifyNever(IOTHubsMainPage.user.auth.signOut(param: anyNamed('param')));
   });
 }
